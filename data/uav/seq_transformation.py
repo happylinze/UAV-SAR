8e643d0742ec84c3afa058463cd67a41ba003a66
ben0i0d <ben0i0d@foxmail.com>
2024-07-07 21:44:20 +0800

    update data-process(speeder 40x)

--- data/uav/seq_transformation.py
+++ data/uav/seq_transformation.py
@@ -237,7 +237,7 @@ if __name__ == '__main__':
     label = np.loadtxt(label_file, dtype=int)  # action label: 0~154
 
     frames_cnt = np.loadtxt(frames_file, dtype=int)  # frames_cnt
-    skes_name = np.loadtxt(skes_name_file, dtype=np.string_)
+    skes_name = np.loadtxt(skes_name_file, dtype=np.bytes_)
 
     with open(raw_skes_joints_pkl, 'rb') as fr:
         skes_joints = pickle.load(fr)  # a list
